397abcb3868efd5b77f45106c1237493c105929f
Trym Tengesdal <tengesdal1994@gmail.com>
2025-11-28 23:12:25 +0100

    Add in updated rust-based rrt-star-lib package and cleanup BG-test

--- tests/test_behavior_generator.py
+++ tests/test_behavior_generator.py
@@ -3,16 +3,22 @@ Test file showing how the behavior generator can be used to generate scenarios.
 """
 
 import colav_simulator.common.paths as dp
-from colav_simulator.behavior_generator import BehaviorGenerationMethod, RRTBehaviorSamplingMethod
+from colav_simulator.behavior_generator import (
+    BehaviorGenerationMethod,
+    RRTBehaviorSamplingMethod,
+)
 from colav_simulator.scenario_generator import Config, ScenarioGenerator
 
 
 def test_behavior_generator() -> None:
     sg_config = Config()
-    sg_config.behavior_generator.ownship_method = BehaviorGenerationMethod.ConstantSpeedAndCourse
+    sg_config.manual_episode_accept = False
+    sg_config.behavior_generator.ownship_method = (
+        BehaviorGenerationMethod.ConstantSpeedAndCourse
+    )
     sg_config.behavior_generator.target_ship_method = (
         BehaviorGenerationMethod.RRTStar
-    )  # NOTE: Remember to install the rrt-rs package first.
+    )  # NOTE: Remember to install the rrt-star-lib package first.
     sg_config.behavior_generator.target_ship_rrt_behavior_sampling_method = (
         RRTBehaviorSamplingMethod.OwnshipWaypointCorridor
     )
@@ -24,7 +30,7 @@ def test_behavior_generator() -> None:
     scenario_episode_list, scenario_enc = scenario_generator.generate(
         config_file=dp.scenarios / "boknafjorden_generation_test.yaml",
         new_load_of_map_data=True,
-        show_plots=True,
+        show_plots=False,
         n_episodes=1,
     )
 
